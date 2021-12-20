184c16532e74e47ac3696b0d269045f827a0513b
SalGalina <salgalina@gmail.com>
2021-12-20 14:15:03 +0300

    add default=yamdb

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': os.getenv(
             'DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv(
-            'POSTGRES_DB'),
+            'POSTGRES_DB', default='yamdb'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
